cf4a96b3528906bb808c066ad2a0273d0d968832
Dariusz Okonski <darek200180@gmail.com>
2022-09-27 22:41:05 +0200

    update styling

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDb = async () => {
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, vel, labore quisquam dignissimos odit porro reiciendis nostrum libero cumque obcaecati officia.',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, vel, labore quisquam dignissimos odit porro reiciendis nostrum libero dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, cumque obcaecati officia.',
             price
         })
 
