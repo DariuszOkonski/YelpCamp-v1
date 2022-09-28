c711427d081c09fcc0bc32a5023d0c09bbef47bf
Dariusz Okonski <darek200180@gmail.com>
2022-09-28 19:19:57 +0200

    style show page

--- seeds/index.js
+++ seeds/index.js
@@ -1,36 +1,37 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
-})
+  console.log("Database connected");
+});
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDb = async () => {
-    await Campground.deleteMany({});
-    
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, vel, labore quisquam dignissimos odit porro reiciendis nostrum libero dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, cumque obcaecati officia.',
-            price
-        })
+  await Campground.deleteMany({});
 
-        await camp.save();        
-    }
-}
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, vel, labore quisquam dignissimos odit porro reiciendis nostrum libero dolor sit amet consectetur adipisicing elit. Labore deleniti esse nam vel dicta? Quasi odio nesciunt, cumque obcaecati officia.",
+      price,
+    });
+
+    await camp.save();
+  }
+};
 
 seedDb().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
